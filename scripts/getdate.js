4438754573c48e0a349e7384501facd39bb6ce65
jasperyaona <jyaona@byupathway.edu>
2026-07-02 22:14:16 +0800

    addes base.css and getdate.js

---
+++ scripts/getdate.js
@@ -0,0 +1,8 @@
+const year = document.querySelector("#currentyear");
+
+
+const today = new Date();
+
+year.innerHTML = today.getFullYear();
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
